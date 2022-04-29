edf20b4c852b802ddb2977923f27afff7deb88bc
Daniil Panov <from4life@pm.me>
2022-04-29 13:42:21 +0700

    Add function that removes hover event and left the same color on the hovered square

--- js-file.js
+++ js-file.js
@@ -4,6 +4,7 @@ const grid = document.querySelector(".grid");
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     square.classList.add('lil-square');
+    // square.classList.add(`${i}`);
     // square.innerText = "Hey ya";
     // square.style.backgroundColor = 'aquamarine';
     // square.style.border = "2px solid black";
@@ -17,17 +18,81 @@ for (let i = 0; i < 256; i++) {
 //color the squares when hovered
 let lilSquares = document.querySelectorAll(".lil-square");
 
-lilSquares.forEach(lilSquare => lilSquare.addEventListener('mouseover', () => {
+function randomColor(e) {
     const hue = Math.floor(Math.random() * 361).toString();
     const saturation = (Math.floor(Math.random() * 56) + 45).toString();
     const lightness = (Math.floor(Math.random() * 76) + 25).toString();
-    lilSquare.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}% )`;
     
+    // lilSquare.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}%)`;
+    // lilSquare.classList.add('hovered');
+    e.target.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}%)`;
+    console.log(hue, saturation, lightness);
+    e.target.classList.add('hovered');
+    
+    if (e.target.classList.contains('hovered')) {
+        removeEvent();
+        e.target.classList.remove('hovered');
+    }
+    console.log(e.target.style.backgroundColor);
+    let regex = /\d{3}|\d{2}|\d{1}/g;
+    let found = (e.target.style.backgroundColor).match(regex);
+    console.log(found);
+    
+    console.log(rgbToHsl(found[0],found[1],found[2]));
+}
+
+function rgbToHsl(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [Math.floor(h * 360), Math.floor(s * 100), Math.floor(l * 100)];
+}
+
+
+
+let i = 0;
+
+lilSquares.forEach(lilSquare => lilSquare.addEventListener('mouseover', randomColor));
+
+function removeEvent () {
+    let lilSquare = document.querySelector('.hovered');
+    lilSquare.removeEventListener('mouseover', randomColor);
+}
+
+
+        // if (lilSquare.classList.contains('hovered')) {
+        //      i++;
+        //      lilSquare.classList.add(`${i}`);
+        // }
+        // console.log(i);
+        // if (lilSquare.classList.contains(/\d/)) {
+        //      lilSquare.removeEventListener('mouseover', randomColor);
+        // }
+
+
+    // const hue = Math.floor(Math.random() * 361).toString();
+    // const saturation = (Math.floor(Math.random() * 56) + 45).toString();
+    // const lightness = (Math.floor(Math.random() * 76) + 25).toString();
+    // lilSquare.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}%)`;
+
     // const randomColor = Math.floor(Math.random()*16777215).toString(16); //parameter '16' converts number to hexadecimal string
     // lilSquare.style.backgroundColor = "#" + randomColor;
 
     // lilSquare.classList.add('hovered');
-}))
 
 function colorGrid () {
     let lilSquares = document.querySelectorAll(".lil-square");
@@ -36,9 +101,12 @@ function colorGrid () {
         const hue = Math.floor(Math.random() * 361).toString();
         const saturation = (Math.floor(Math.random() * 56) + 45).toString();
         const lightness = (Math.floor(Math.random() * 76) + 25).toString();
-        lilSquare.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}% )`;
-    // lilSquare.classList.add('hovered');
+        lilSquare.style.backgroundColor =`hsl(${hue},${saturation}%,${lightness}%)`;
+     // lilSquare.classList.add('hovered');
 }))
+    // lilSquares.forEach(lilSquare => lilSquare.addEventListener('mouseover', () => {
+    //     lilSquare.style.backgroundColor = `hsl`
+    // })
 }
 
 //clean the hovering effect
